2bb4e5bb46a0d60997821dd0aa0541238d148fef
Faisal Muzaffar <258183906+erfaisal@users.noreply.github.com>
2026-07-22 04:42:16 +0530

    Create database.ts

---
+++ src/types/database.ts
@@ -0,0 +1,7 @@
+// NOTE: This is a temporary placeholder to prevent TypeScript errors.
+// Once your Supabase project is created, overwrite this file by running:
+// npx supabase gen types typescript --project-id "YOUR_PROJECT_REF" > src/types/database.ts
+
+export type Database = any;
+export type Tables<T extends string> = any;
+export type Enums<T extends string> = any;
